d5c6748f67c1a8d2076d92f7ebb411bc4a7405c0
Jason Reilly <jdr0887@gmail.com>
2017-04-06 10:05:38 -0400

    regenerating dot

--- ncnexus38-merge-vc-workflow/src/test/java/edu/unc/mapseq/workflow/NCNEXUSMergeBAMWorkflowTest.java
+++ ncnexus38-merge-vc-workflow/src/test/java/edu/unc/mapseq/workflow/NCNEXUSMergeBAMWorkflowTest.java
@@ -3,6 +3,8 @@ package edu.unc.mapseq.workflow;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.jgrapht.DirectedGraph;
 import org.jgrapht.graph.DefaultDirectedGraph;
@@ -14,13 +16,21 @@ import org.renci.jlrm.condor.ext.CondorDOTExporter;
 import org.renci.jlrm.condor.ext.CondorJobVertexNameProvider;
 
 import edu.unc.mapseq.module.core.RemoveCLI;
-import edu.unc.mapseq.module.sequencing.gatk2.GATKDepthOfCoverageCLI;
-import edu.unc.mapseq.module.sequencing.gatk2.GATKUnifiedGenotyperCLI;
+import edu.unc.mapseq.module.core.ZipCLI;
+import edu.unc.mapseq.module.sequencing.SureSelectTriggerSplitterCLI;
+import edu.unc.mapseq.module.sequencing.filter.FilterVariantCLI;
+import edu.unc.mapseq.module.sequencing.freebayes.FreeBayesCLI;
+import edu.unc.mapseq.module.sequencing.gatk3.GATKVariantAnnotatorCLI;
 import edu.unc.mapseq.module.sequencing.picard.PicardAddOrReplaceReadGroupsCLI;
 import edu.unc.mapseq.module.sequencing.picard.PicardMarkDuplicatesCLI;
 import edu.unc.mapseq.module.sequencing.picard.PicardMergeSAMCLI;
+import edu.unc.mapseq.module.sequencing.picard2.PicardSortVCFCLI;
+import edu.unc.mapseq.module.sequencing.samtools.SAMToolsDepthCLI;
 import edu.unc.mapseq.module.sequencing.samtools.SAMToolsFlagstatCLI;
 import edu.unc.mapseq.module.sequencing.samtools.SAMToolsIndexCLI;
+import edu.unc.mapseq.module.sequencing.vcflib.MergeVCFCLI;
+import edu.unc.mapseq.module.sequencing.vcflib.SortAndRemoveDuplicatesCLI;
+import edu.unc.mapseq.module.sequencing.vcflib.VCFFilterCLI;
 
 public class NCNEXUSMergeBAMWorkflowTest {
 
@@ -35,8 +45,9 @@ public class NCNEXUSMergeBAMWorkflowTest {
 
         DirectedGraph<CondorJob, CondorJobEdge> graph = new DefaultDirectedGraph<CondorJob, CondorJobEdge>(CondorJobEdge.class);
 
-        // new job
-        CondorJob mergeBAMFilesJob = new CondorJobBuilder().name(String.format("%s_%d", PicardMergeSAMCLI.class.getSimpleName(), ++count)).build();
+        // // new job
+        CondorJob mergeBAMFilesJob = new CondorJobBuilder().name(String.format("%s_%d", PicardMergeSAMCLI.class.getSimpleName(), ++count))
+                .build();
         graph.addVertex(mergeBAMFilesJob);
 
         // new job
@@ -52,35 +63,97 @@ public class NCNEXUSMergeBAMWorkflowTest {
         graph.addEdge(picardAddOrReplaceReadGroupsJob, picardMarkDuplicatesJob);
 
         // new job
-        CondorJob samtoolsIndexJob = new CondorJobBuilder().name(String.format("%s_%d", SAMToolsIndexCLI.class.getSimpleName(), ++count)).build();
+        CondorJob samtoolsIndexJob = new CondorJobBuilder().name(String.format("%s_%d", SAMToolsIndexCLI.class.getSimpleName(), ++count))
+                .build();
         graph.addVertex(samtoolsIndexJob);
         graph.addEdge(picardMarkDuplicatesJob, samtoolsIndexJob);
 
         // new job
-        CondorJob samtoolsFlagstatJob = new CondorJobBuilder().name(String.format("%s_%d", SAMToolsFlagstatCLI.class.getSimpleName(), ++count))
-                .build();
+        CondorJob samtoolsFlagstatJob = new CondorJobBuilder()
+                .name(String.format("%s_%d", SAMToolsFlagstatCLI.class.getSimpleName(), ++count)).build();
         graph.addVertex(samtoolsFlagstatJob);
         graph.addEdge(samtoolsIndexJob, samtoolsFlagstatJob);
 
         // new job
+        CondorJob zipJob = new CondorJobBuilder().name(String.format("%s_%d", ZipCLI.class.getSimpleName(), ++count)).build();
+        graph.addVertex(zipJob);
+        graph.addEdge(samtoolsIndexJob, zipJob);
+
+        // new job
+        CondorJob samtoolsDepthJob = new CondorJobBuilder().name(String.format("%s_%d", SAMToolsDepthCLI.class.getSimpleName(), ++count))
+                .build();
+        graph.addVertex(samtoolsDepthJob);
+        graph.addEdge(samtoolsIndexJob, samtoolsDepthJob);
+
+        // new job
+        CondorJob picardCollectHsMetricsJob = new CondorJobBuilder().name(String.format("%s_%d", ZipCLI.class.getSimpleName(), ++count))
+                .build();
+        graph.addVertex(picardCollectHsMetricsJob);
+        graph.addEdge(samtoolsIndexJob, picardCollectHsMetricsJob);
+
+        // new job
+        CondorJob sureSelectTriggerSplitterJob = new CondorJobBuilder()
+                .name(String.format("%s_%d", SureSelectTriggerSplitterCLI.class.getSimpleName(), ++count)).build();
+        graph.addVertex(sureSelectTriggerSplitterJob);
+        graph.addEdge(samtoolsIndexJob, sureSelectTriggerSplitterJob);
+
+        List<CondorJob> mergeVCFParentJobs = new ArrayList<CondorJob>();
+
+        for (int i = 0; i < 4; i++) {
+
+            // new job
+            CondorJob freeBayesJob = new CondorJobBuilder().name(String.format("%s_%d", FreeBayesCLI.class.getSimpleName(), ++count))
+                    .build();
+            graph.addVertex(freeBayesJob);
+            graph.addEdge(sureSelectTriggerSplitterJob, freeBayesJob);
+            mergeVCFParentJobs.add(freeBayesJob);
+
+        }
+
+        // new job
+        CondorJob mergeVCFJob = new CondorJobBuilder().name(String.format("%s_%d", MergeVCFCLI.class.getSimpleName(), ++count)).build();
+        graph.addVertex(mergeVCFJob);
+        for (CondorJob job : mergeVCFParentJobs) {
+            graph.addEdge(job, mergeVCFJob);
+        }
+
+        // new job
+        CondorJob vcfFilterJob = new CondorJobBuilder().name(String.format("%s_%d", VCFFilterCLI.class.getSimpleName(), ++count)).build();
+        graph.addVertex(vcfFilterJob);
+        graph.addEdge(mergeVCFJob, vcfFilterJob);
+
+        // new job
+        CondorJob sortAndRemoveDuplicatesJob = new CondorJobBuilder()
+                .name(String.format("%s_%d", SortAndRemoveDuplicatesCLI.class.getSimpleName(), ++count)).build();
+        graph.addVertex(sortAndRemoveDuplicatesJob);
+        graph.addEdge(vcfFilterJob, sortAndRemoveDuplicatesJob);
+
+        // new job
+        CondorJob picardSortVCFJob = new CondorJobBuilder().name(String.format("%s_%d", PicardSortVCFCLI.class.getSimpleName(), ++count))
+                .build();
+        graph.addVertex(picardSortVCFJob);
+        graph.addEdge(sortAndRemoveDuplicatesJob, picardSortVCFJob);
+
+        // new job
+        CondorJob gatkVCFJob = new CondorJobBuilder().name(String.format("%s_%d", GATKVariantAnnotatorCLI.class.getSimpleName(), ++count))
+                .build();
+        graph.addVertex(gatkVCFJob);
+        graph.addEdge(picardSortVCFJob, gatkVCFJob);
+
+        // new job
+        CondorJob filterVariantJob = new CondorJobBuilder().name(String.format("%s_%d", FilterVariantCLI.class.getSimpleName(), ++count))
+                .build();
+        graph.addVertex(filterVariantJob);
+        graph.addEdge(gatkVCFJob, filterVariantJob);
+
+        // new job
         CondorJob removeJob = new CondorJobBuilder().name(String.format("%s_%d", RemoveCLI.class.getSimpleName(), ++count)).build();
         graph.addVertex(removeJob);
-        graph.addEdge(samtoolsFlagstatJob, removeJob);
-
-        // new job
-        CondorJob depthOfCoverageJob = new CondorJobBuilder().name(String.format("%s_%d", GATKDepthOfCoverageCLI.class.getSimpleName(), ++count))
-                .build();
-        graph.addVertex(depthOfCoverageJob);
-        graph.addEdge(samtoolsIndexJob, depthOfCoverageJob);
-
-        // new job
-        CondorJob unifiedGenotyperJob = new CondorJobBuilder().name(String.format("%s_%d", GATKUnifiedGenotyperCLI.class.getSimpleName(), ++count))
-                .build();
-        graph.addVertex(unifiedGenotyperJob);
-        graph.addEdge(depthOfCoverageJob, unifiedGenotyperJob);
+        graph.addEdge(filterVariantJob, removeJob);
 
         CondorJobVertexNameProvider vnp = new CondorJobVertexNameProvider();
-        CondorDOTExporter<CondorJob, CondorJobEdge> dotExporter = new CondorDOTExporter<CondorJob, CondorJobEdge>(vnp, vnp, null, null, null, null);
+        CondorDOTExporter<CondorJob, CondorJobEdge> dotExporter = new CondorDOTExporter<CondorJob, CondorJobEdge>(vnp, vnp, null, null,
+                null, null);
         File srcSiteResourcesImagesDir = new File("../src/site/resources/images");
         if (!srcSiteResourcesImagesDir.exists()) {
             srcSiteResourcesImagesDir.mkdirs();
